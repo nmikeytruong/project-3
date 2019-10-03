56349594005b372f6bd8ca7cf3ba8cbcc6c33168
Toby Jackson <tobyajackson@gmail.com>
2019-10-02 21:39:57 -0400

    wire-up db, model, controller, and routes

---
+++ routes/api/profiles.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+const profilesController = require("../../controllers/profileController");
+
+// Matches with "/api/profiles"
+router.route("/").get(profilesController.findAll);
+
+// Matches with "POST /api/profiles"
+router.route("/").post(profilesController.create);
+
+// Matches with "/api/profiles/:id"
+router.route("/:id").get(profilesController.findById);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");
 
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +11,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(routes);
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/profilesdb",
